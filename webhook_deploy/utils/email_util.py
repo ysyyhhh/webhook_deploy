d4a7af661a412b25245f2805589082b5ba161e14
ysyyhhh <757578166@qq.com>
2023-09-15 09:35:18 +0800

    exception

--- webhook_deploy/utils/email_util.py
+++ webhook_deploy/utils/email_util.py
@@ -28,29 +28,18 @@ def send_email(receivers:list,content:str,subject:str):
         res = smtpObj.login(mail_user,mail_pass)  
         smtpObj.sendmail(sender, receivers, message.as_string())
         logger.info("邮件发送成功")
-    except smtplib.SMTPException:
+    except Exception as e:
+        logger.info(e)
         logger.info("Error: 无法发送邮件")
-
-def send_test_result_email(update_time:str,username:str,branch:str, email:str,result):
-    if result == 1:
-        subject = "测试失败！" + username + "，您刚刚提交的分支{}测试失败，请检查并修复后再提交".format(branch)
-        content = username + "，您刚刚提交的分支{}在{}测试失败，请检查并修复后再提交".format(branch,update_time)
-    else:
-        subject = "测试通过！" + username + "，您刚刚提交的分支{}测试成功，请提交pull request 并 通知相关人员进行代码review".format(branch)
-        content = username + "，您刚刚提交的分支{}在{}测试成功，请提交pull request 并 通知相关人员进行代码review".format(branch,update_time)
-    logger.info(email)
-    logger.info(subject)
-    logger.info(content)
-    send_email([email],content,subject)
-    return result
 
 if __name__ == '__main__':
     email = "757578166@qq.com"
     branch = "rongrunxiang/click_active_cal"
     username = "ysyy"
     update_time = "2023-07-11T14:43:22+08:00"
-    test_and_send_email(update_time,username,branch,email)
-    # receivers = ['757578166@qq.com']
-    # content = '自动化邮件发送测试...'
-    # subject = 'git提交后的邮件检查结果发送测试'
-    # send_email(receivers,content,subject)
+    # send_email(update_time,username,branch,email)
+    receivers = ['757578166@qq.com']
+    content = '''ysyyhhh，您刚刚在DigitalMap项目中提交的分支wds/webhook_test在2023-09-15T09:17:45+08:00测试成功，请提交pull request 并 通知相关人员进行代码review
+详情见 https://git.mooctest.com/digital-navigation-map/DigitalMap/compare/2608c36ee44269f26f7eae0830113523162842ef...16d08c0e49175d390b5b39058eaf5ae668aa3df0'''
+    subject = 'git提交后的邮件检查结果发送测试'
+    send_email(receivers,content,subject)
