1f7ea6a094b30051c440e96a9e9a17fc6f534779
ysyyhhh <757578166@qq.com>
2023-09-15 09:57:44 +0800

    test

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 docker stop webhook_deploy
 docker rm webhook_deploy
 docker build -t webhook_deploy .
-docker run --network host -d --name webhook_deploy --env-file .env -p 8078:8078 webhook_deploy
+docker run --network=host -d --name webhook_deploy --env-file .env -p 8078:8078 webhook_deploy
